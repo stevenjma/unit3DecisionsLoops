bd17db6faf83e9ca49cf29fbdb7f456cb826680e
stevenjma <sjma@NN123-HA-18.HS.sd203.org>
2015-11-03 14:12:36 -0600

    Finished up all of the all Chapter 6 programs except vampirenumbers...stupid vampire numbers.

--- Chapter 6 Practice/MonteCarlo.java
+++ Chapter 6 Practice/MonteCarlo.java
@@ -8,12 +8,23 @@ public class MonteCarlo
         System.out.println("Enter the radius of your circle.");
         double radius = s.nextDouble();
         double quarterofarea = (Math.pow(radius,2) * Math.PI)/4.0;
-        double restofsquare = Math.pow(radius,2) - quarterofarea;
+        double a = quarterofarea * 4;
         double square = Math.pow(radius,2);
         double proportion = quarterofarea / square;
-        for (int count = 0; count != 1000; count++)
+        double n = 0;
+        for (int count = 0; count != 1000000; count++)
         {
             double random = Math.random();
+            if (random < proportion)
+            {
+                n++;
+            }
         }
+        double calculatedproportion = n/1000000;
+        double calculatedarea = calculatedproportion * square * 4;
+        double calculatedpi = calculatedproportion * 4;
+        System.out.println("Projected Area: " + calculatedarea);
+        System.out.println("Actual Area: " + a);
+        System.out.println("Projected Value of Pi: " + calculatedpi);
     }
 }
